a406ceb1259781f985bb8d2fabf468aec2965c9c
Sergius-hub <sergiusbox@mail.ru>
2026-06-02 03:16:53 +0300

    fix

--- src/adapters.py
+++ src/adapters.py
@@ -1,28 +1,44 @@
 from requests import get
 from src.base_api import BaseAPI
 
-class AdapterNominative(BaseAPI):
 
-    def __init__(self, url: str, headers: dict, params: dict):
+class Adapter(BaseAPI):
+    def __init__( self, url: str, headers: dict, params: dict ):
         super().__init__(url, headers, params)
-
-    @classmethod
-    def set_request(cls, url: str, headers: dict, params: dict):
-        return cls(url, headers, params)
 
     def get_response( self ):
         return get(url=self.url, headers=self.headers, params=self.params)
 
-class AdapterOpensky(BaseAPI):
+class AdapterNominative(Adapter):
 
-    def __init__( self, url: str, headers: dict, params: dict ):
-        super().__init__( url, headers, params )
+    def __init__( self, country: str):
+        super().__init__(
+            url="https://nominatim.openstreetmap.org/search",
+            headers={
+                "User-Agent": "test-app/1.0"
+            },
+            params={
+                "country": country,
+                "format": 'json',
+                "limit": 1,
+            }
+        )
 
-    @classmethod
-    def set_request( cls, url: str, headers: dict, params: dict ):
-        return cls( url, headers, params )
 
-    def get_response( self ):
-        return get( url=self.url, headers=self.headers, params=self.params )
+class AdapterOpensky(Adapter):
 
+    def __init__( self, data: list):
+
+        geo_coordinates = data[0].get( "boundingbox" )
+        params = {
+            'lamin': geo_coordinates[0],
+            'lamax': geo_coordinates[1],
+            'lomin': geo_coordinates[2],
+            'lomax': geo_coordinates[3],
+        }
+
+        super().__init__(
+            url="https://opensky-network.org/api/states/all?",
+            params=params
+        )
 
--- src/base_api.py
+++ src/base_api.py
@@ -2,14 +2,11 @@ from abc import ABC, abstractmethod
 
 class BaseAPI(ABC):
 
-	def __init__(self, url: str, headers: dict, params: dict):
+	def __init__(self, url: str, headers: dict | None = None, params: dict | None = None):
 		self.url = url
 		self.headers = headers
 		self.params = params
 
-	@abstractmethod
-	def set_request( self, *args, **kwargs ):
-		pass
 
 	@abstractmethod
 	def get_response( self, *args, **kwargs ):
