820e82dd192fff3a9a215b73ca842e4ab2430e17
Sergius <sergiusbox@mail.ru>
2026-06-05 17:54:17 +0300

    ierarch

--- src/adapter_api.py
+++
@@ -1,7 +0,0 @@
-from src.base_api import BaseAPI
-
-class AdapterAPI(BaseAPI):
-    def __init__(self):
-        super().__init__(url, headers, params)
-
-
--- src/base_aeroplane.py
+++
@@ -1,4 +0,0 @@
-
-
-class BaseAeroplane:
-    pass
