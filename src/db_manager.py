f38390275ccc78c3facaa8aaaa8b75c379210a7d
Sergius-hub <sergiusbox@mail.ru>
2026-06-18 07:47:03 +0300

    connector

--- src/db_manager.py
+++ src/db_manager.py
@@ -1,13 +1,29 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+
+
+class BaseDBConnector(ABC):
+
+    @abstractmethod
+    def connect( self ):
+        pass
+
+
+class DBConnector(BaseDBConnector):
+    pass
 
 
 class BaseDBManager(ABC):
+    pass
 
-    def get_countries_and_aeroplanes_count(self):
+
+
+class DBManager(BaseDBManager):
+
+    def get_countries_and_aeroplanes_count( self ):
         """Получает список всех стран и количество самолетов в их воздушных пространствах"""
         pass
 
-    def get_all_aeroplanes(self):
+    def get_all_aeroplanes( self ):
         """Получает список всех воздушных судов"""
         pass
 
@@ -22,7 +38,3 @@ class BaseDBManager(ABC):
     def get_aeroplanes_with_keyword( self ):
         """Получает список всех самолетов, в позывном которых содержатся переданные в метод символы"""
         pass
-
-
-class DBManager(BaseDBManager):
-    pass
